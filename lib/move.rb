98bda568cb824e292ba484c1a89755cb7e4f0520
trenyik <trenyik.nikolett1@gmail.com>
2019-08-08 09:32:17 +0100

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -6,4 +6,20 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
-# code your input_to_index and move method here!
+def input_to_index (input)
+input.to_i - 1 
+end
+
+def move (board, index, value="X")
+  board [index] = value
+end
+
+def number_adder(n)
+  n += 10
+end
+
+def array_adder(a)
+  a << "new object"
+end
+
+
