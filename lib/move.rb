28d15eb9c52dfd00679bfc66a7e0246f3b9bb68b
Kevin <degouveiakevin@gmail.com>
2019-08-08 12:20:25 +0100

    Done.

--- lib/move.rb
+++ lib/move.rb
@@ -1,3 +1,7 @@
+def greet
+  puts "Welcome to Tic Tac Toe!"
+end
+
 def display_board(board)
   puts " #{board[0]} | #{board[1]} | #{board[2]} "
   puts "-----------"
@@ -6,4 +10,26 @@ def display_board(board)
   puts " #{board[6]} | #{board[7]} | #{board[8]} "
 end
 
+def input_to_index(input)
+ # begin
+  #input = Integer(input)
+  #rescue ArgumentError
+  #puts "Please enter an integer number"
+  #input = gets.strip
+  #retry
+  #end
+  #if input > 9 or input < 1
+   # puts "Please make sure the number is between 1 - 9"
+    #input = gets.chomp
+    #input_to_index(input)
+  #end
+  position = input.to_i - 1
+  return position
+end
+
+def move(board, position, char = "X")
+
+  board[position] = char
+  return board
+end
 # code your input_to_index and move method here!
